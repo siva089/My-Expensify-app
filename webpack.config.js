a08980eb3e12090b2299444dc4c6ae3a7af247ac
siva sankar <sivasankarreddyt089@gmail.com>
2019-03-13 12:46:24 +0530

    production build changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
-const path=require('path')
-module.exports={
+const path=require('path');
+const ExtractTextPlugin=require('extract-text-webpack-plugin')
+
+module.exports=(env)=>{
+    const isProduction=env==='production';
+    const CSSExtract=new ExtractTextPlugin('style.css')
+    return{
     entry:'./src/app.js',
     output:{
         path:path.join(__dirname,'public'),
@@ -15,18 +20,33 @@ module:{
     },
     {
         test:/\.s?css$/,
-        use:[
-            'style-loader',
-            'css-loader',
-            'sass-loader'
-        ]
+        use:CSSExtract.extract({
+            use:[
+                {
+                    loader:'css-loader',
+                    options:{
+                        sourceMap:true
+                    }
+                },
+                {
+                    loader:'sass-loader',
+                    options:{
+                        sourceMap:true
+                    }
+                }
+            ]
+        })
     }
 ]
 },
-devtool:'cheap-module-eval-source-map',
+plugins:[
+    CSSExtract
+],
+devtool:isProduction?'source-map':'inline-source-map',
 devServer:{
     contentBase: path.join(__dirname, 'public'),
     historyApiFallback:true
 }
 
+}
 }
